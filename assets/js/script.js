f54f08cff86281fe35a268e5fe60a60ed4a0b1d9
Nany <lesfarguettes@free.fr>
2023-04-17 19:37:33 +0200

    modif_img_singe_divers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-const APIKEY = '3215414c948b66e7633fdd873312bc11';
+//la clef APIKEY unique en cas de besoin : '3215414c948b66e7633fdd873312bc11';
 
-// Appel à l'API OpewWeather avec ville en paramère de fonction
+// Appel à l'API OpewWeather avec ville en paramère de fonction grâce à l'url
 let apiCall = function (city) {
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=3215414c948b66e7633fdd873312bc11&units=metric';
     
-
+// On souhaite récupérer les données du fichier JSON pour pouvoir les afficher ensuite sur le DOM
     fetch(url)
         .then((response) =>
             response.json().then((data) => {
                 console.log(data);
+// On récupère les différentes ID pour pouvoir y placer ensuite les différentes données 
                 document.querySelector('#city').innerHTML = data.name;
                 document.querySelector('#temp').innerHTML =
                     "<i class='fas fa-thermometer-half'</i>" + ' ' + data.main.temp + ' °';
@@ -21,12 +22,13 @@ let apiCall = function (city) {
         .catch((err) => console.log('Erreur : ' + err));
 };
 
-// Ecouteur d'évènement sur la soumission du formulaire 
+// Ecouteur d'évènement sur la soumission du formulaire (il faut casser l'évènement par défaut consistant
+//  à raffraichir la page lorsqu"on clique sur "rechercher" avec la function e.preventDefault ) 
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
     let ville = document.querySelector('#inputCity').value;
     apiCall(ville);
 });
 
-// Appel par défaut au chargement de la page
+// Appel à une ville par défaut au chargement de la page
 apiCall('padirac');
